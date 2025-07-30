a8fc5f98d6722ceb6aae8820d50f35a972ee6d03
Marlon Harrison <92547717+marlon-ionic@users.noreply.github.com>
2025-07-30 08:33:36 -0500

    downloadAndOpenLocalPath

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { Directory, Filesystem } from '@capacitor/filesystem';
+import { FileTransfer } from '@capacitor/file-transfer';
+import { FileViewer } from '@capacitor/file-viewer';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
 })
 export class HomePage {
-  constructor() {}
+  constructor() { }
+
+  async downloadAndOpenLocalPath() {
+    const url = new URL('https://www.rd.usda.gov/sites/default/files/pdf-sample_0.pdf');
+
+    const filesystemUri = await Filesystem.getUri({
+      directory: Directory.Documents,
+      path: url.pathname
+    });
+
+    console.log('uri', filesystemUri.uri);
+
+    const result = await FileTransfer.downloadFile({
+      url: url.href,
+      path: filesystemUri.uri
+    });
+    console.log('downloaded', result);
+
+    try {
+      if (result && result.path) {
+        console.log('** opening: (localPath) **', result.path);
+        await FileViewer.openDocumentFromLocalPath({
+          path: result.path
+        });
+      } else {
+        console.error("No path from FileTransfer.downloadFile");
+      }
+    } catch (e) {
+      console.error('caught error opening local path', e);
+    }
+  }
 }
