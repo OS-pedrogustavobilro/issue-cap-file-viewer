32edd3dac85890a08843cd6e852ebc5b084816b2
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2025-10-01 12:31:29 +0100

    chore: Add option to test previewMediaContentFromLocalPath

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -42,4 +42,34 @@ export class HomePage {
       console.error('caught error opening local path', e);
     }
   }
+
+  async downloadAndPreviewLocalPath() {
+    const url = new URL('https://archive.org/download/sample-mp4-file_202101/sample-mp4-file.mp4');
+
+    const filesystemUri = await Filesystem.getUri({
+      directory: Directory.Documents,
+      path: url.pathname
+    });
+
+    console.log('uri', filesystemUri.uri);
+
+    const result = await FileTransfer.downloadFile({
+      url: url.href,
+      path: filesystemUri.uri
+    });
+    console.log('downloaded', result);
+
+    try {
+      if (result && result.path) {
+        console.log('** preview media: (localPath) **', result.path);
+        await FileViewer.previewMediaContentFromLocalPath({
+          path: result.path
+        });
+      } else {
+        console.error("No path from FileTransfer.downloadFile");
+      }
+    } catch (e) {
+      console.error('caught error preview media local path', e);
+    }
+  }
 }
